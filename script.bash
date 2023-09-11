57c9f9043aab4ec77aba1b40835489b7263e9379
Shashwat Pandey <shashwatp145@gmail.com>
2023-09-11 15:54:47 +0530

    Update script.bash

--- script.bash
+++ script.bash
@@ -1,20 +1,21 @@
 #!/bin/bash
 N=$1
-if [ $N -lt 10 ]
+if [ $N -lt 10 ]                                    //If n is less than 10
 then
         OUT=$((N*N))
-elif [ $N -ge 10 ] && [ $N -le 20 ]
+elif [ $N -gt 10 ] && [ $N -lt 20 ]  //BUG 1       (If n is between 10 and 20(exclusive))
 then
         OUT=1
         LIM=$((N - 10))
-        for (( i=1; i<$LIM; i++ ))
+        for (( i=1; i<=$LIM; i++ ))  //BUG 2
         do
                 OUT=$((OUT * i))
         done
-else
+else                                                //If n is greater than 20
+
         LIM=$((N - 20))
         OUT=$((LIM * LIM))
-        OUT=$((OUT + LIM))
+        OUT=$((OUT + LIM))           //BUG 3
         OUT=$((OUT / 2))
 fi
 echo $OUT
